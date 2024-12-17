53cac319ee9b2bfaf75f3426db90f7586b2f0a10
Lydia S <149545941+LISims88@users.noreply.github.com>
2024-12-17 12:04:46 -0600

    Add description to the app.

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <h2>Welcome to Cozy Creations Home Goods</h2>
+        <p>Our mission is to provide you with the best homemade goods that are both cozy and stylish. Shop our products today and make your home a cozy creation.</p>
+      </main>
     </div>
   );
 
